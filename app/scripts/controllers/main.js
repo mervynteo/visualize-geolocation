86ea210018d7f8435915c171f459ce2e90ed1c29
Theodore <theo@Theodores-MacBook-Pro.local>
2014-04-17 23:21:37 -0700

    CR of server working. Also created simple text form on client. using angular resource

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 angular.module('uberLocationApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $resource) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
+
+	$scope.locationParam = {};
+	
+	$scope.locationCreate = function(form) {
+		console.log($scope.locationParam);
+		var LocationResource = $resource('api/locations');
+		var newLocation = new LocationResource($scope.locationParam);
+
+		newLocation.$save();
+		// try using angular resource to submit to rest endpoint here
+	};
   });
